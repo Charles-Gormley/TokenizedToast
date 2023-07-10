b33c47bba86a647392450ce77d418a18f725e55d
Charlie <ceg98@drexel.edu>
2023-07-09 21:34:29 -0400

    Clustering & Encoding Progress

--- Machine-Learning/Content-Encoding/Training/encodewikipedia.py
+++ Machine-Learning/Content-Encoding/Training/encodewikipedia.py
@@ -1,18 +1,25 @@
-from encodings import BERT
+from encodingclasses import BERT
 
 import os
 import pickle
+import random
+
+import logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 wikipedia_corpus_path = 'D:/INFO323/TokenizedToast/corpus-all.pkl'
 
+
+logging.info("Loading wikipedia Corpus")
 if os.path.exists(wikipedia_corpus_path):
     with open(wikipedia_corpus_path, 'rb') as file:
         corpus = pickle.load(file)
 
 encoder = BERT()
-
+logging.info("Encoding Corpus")
 encodings = encoder.encode_corpus(corpus)
 
+logging.info("Mapping Encodings")
 umap_encodings = encoder.umap_embeddings(encodings)
 
 # Save np.ndarray as a pickel file
