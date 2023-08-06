e6a301ad1dcfee97f751d35e609c4da9ed66725a
Charlie Gormley <ceg98@drexel.edu>
2023-08-05 20:41:10 -0400

    Cron job toggling

---
+++ Misc/Helper-Scripts/cron-handler.py
@@ -0,0 +1,13 @@
+import os
+
+CRON_PATHS = ['/etc/cron.d', '/etc/cron.daily', '/etc/cron.hourly', '/etc/cron.weekly']
+
+def disable_cron_jobs():
+    for path in CRON_PATHS:
+        os.system(f'sudo chmod -x {path}')  # remove execute permissions
+
+def enable_cron_jobs():
+    for path in CRON_PATHS:
+        os.system(f'sudo chmod +x {path}')  # add execute permissions
+
+
