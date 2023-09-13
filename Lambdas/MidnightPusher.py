f16b439950dfae41c571a3e225969647417e67d1
Charlie <ceg98@drexel.edu>
2023-09-12 22:22:40 -0400

    Updatintg lambda with flags

--- Lambdas/MidnightPusher.py
+++ Lambdas/MidnightPusher.py
@@ -13,12 +13,12 @@ def lambda_handler(event, context, set_article_num=False):
     main_path = "/home/ec2-user/TokenizedToast/ETL/RSS-Extractor/main.py"
     git_path = "/home/ec2-user/TokenizedToast/Misc/Helper-Scripts/setup_git_ec2.py"
     
-    git_command = 'sudo -u ec2-user /usr/local/bin/python3.11 ' + git_path + ' >> /tmp/git_process.log 2>&1'
+    git_command = f'sudo -u ec2-user /usr/local/bin/python3.11 {git_path} >> /tmp/git_process.log 2>&1'
 
     if set_article_num:
-        main_command = 'sudo -u ec2-user /usr/local/bin/python3.11 ' + main_path + ' >> /tmp/temp.log 2>&1'
+        main_command = f'sudo -u ec2-user /usr/local/bin/python3.11 {main_path} --article_num {set_article_num}  >> /tmp/temp.log 2>&1'
     else: 
-        main_command = 'sudo -u ec2-user /usr/local/bin/python3.11 ' + main_path + ' >> /tmp/temp.log 2>&1'
+        main_command = f'sudo -u ec2-user /usr/local/bin/python3.11 {main_path} >> /tmp/temp.log 2>&1'
     
     # Start the instance
     ec2.start_instances(InstanceIds=[instance_id])
