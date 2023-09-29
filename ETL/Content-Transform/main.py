1aae2aafcc73d1cb73b1ddd382666dab4f45c2f3
Charlie <ceg98@drexel.edu>
2023-09-29 13:11:53 -0400

    debugging

--- ETL/Content-Transform/main.py
+++ ETL/Content-Transform/main.py
@@ -9,9 +9,11 @@ cleaned_data_fn = 'cleaned-data.pkl'
 content_json_fn = 'content.json'
 
 content_json_path = "/home/ec2-user/" + content_json_fn
+cleaned_data_path = "/home/ec2-user/" + cleaned_data_fn
+
 cleaner = Cleaner(content_json_path)
 df = cleaner.clean_data()
 
 
-with open(cleaned_data_fn, 'wb') as f:
+with open(cleaned_data_path, 'wb') as f:
     pickle.dump(df, f)
