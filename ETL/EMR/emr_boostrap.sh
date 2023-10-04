788ac20fdc4216d54756a9e408392d5c3f5dd16a
Charles E. Gormley <76138796+Charles-Gormley@users.noreply.github.com>
2023-10-03 20:48:57 -0400

    numpy change

--- ETL/EMR/emr_boostrap.sh
+++ ETL/EMR/emr_boostrap.sh
@@ -6,7 +6,7 @@ export HADOOP_CONF_DIR=/etc/hadoop/conf
 export SPARK_JARS_DIR=/usr/lib/spark/jars
 export SPARK_HOME=/usr/lib/spark' >> $HOME/.bashrc && source $HOME/.bashrc
 
-sudo python3 -m pip install awscli boto numpy joblib spark-nlp pandas==1.2.5
+sudo python3 -m pip install awscli boto numpy==1.24.1 joblib spark-nlp pandas==1.5.3 -vvv
 
 set +x
 exit 0
