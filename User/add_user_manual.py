a50047118bcb8f112833ef2338c61af66dc840a1
Charlie <ceg98@drexel.edu>
2023-11-06 00:13:28 -0500

    Sample New User

--- User/add_user_manual.py
+++ User/add_user_manual.py
@@ -3,10 +3,12 @@ from user_events import UserStructure
 u = UserStructure()
 
 # Add topics
-email = ''
-name = ''
-preferences = ''
-topic_list = [] # List of topics
+email = 'Zarin.carlos@gmail.com'
+name = 'Carlos'
+preferences = 'Summarize the articles in a morning brew like fashion as in business casual.'
+topic_list = ["Business Compliance & Tech Compliance. Any change in regulations about tech", "Decentralized Blockchain Emerging tech or regulations", "New breakthroughs in Rocket Technology"] # List of topics
 
-user_name, info = u.add_user(email, name, preferences)
+new_user, info = u.add_user(email, name, preferences)
+user_name = new_user['user_id']
+
 u.add_user_interests(name, user_name, topic_list)
