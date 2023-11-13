38f8d5cefa8036a225a864136e5ef7bb4eb20cec
Charles E. Gormley <76138796+Charles-Gormley@users.noreply.github.com>
2023-11-13 01:49:24 -0500

    handy dandy

--- ArchV2/Content-Processor/article_extraction.py
+++ ArchV2/Content-Processor/article_extraction.py
@@ -46,6 +46,7 @@ def extract_feed(rss:dict, output_queue):
         print(feed.keys())
         for entry in feed['entries']:
             print(entry.keys())
+            print(entry['published'])
             pub_date = datetime.strptime(entry['published'], '%a, %d %b %Y %H:%M:%S GMT')
 
             if pub_date > last_date:
