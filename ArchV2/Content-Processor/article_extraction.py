777b1e16d834dda230ae2340d8f79fbfe7ba140e
Charles E. Gormley <76138796+Charles-Gormley@users.noreply.github.com>
2023-11-13 01:46:54 -0500

    maybe?

--- ArchV2/Content-Processor/article_extraction.py
+++ ArchV2/Content-Processor/article_extraction.py
@@ -44,10 +44,10 @@ def extract_feed(rss:dict, output_queue):
     try:
         feed = feedparser.parse(feed_url)
 
-        for entry in feed.entries:
+        for entry in feed['entries']:
             print(entry['published'])
             pub_date = datetime.strptime(entry['published'], '%a, %d %b %Y %H:%M:%S GMT')
-            print(pub_date)
+
             if pub_date > last_date:
                 link = entry.link
                 title, text = extract_article(link)
