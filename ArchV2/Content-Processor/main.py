2e7f1bb519221bc43ffec04fa73328a4121474b9
Charlie <ceg98@drexel.edu>
2023-12-05 02:23:37 -0500

    better test

--- ArchV2/Content-Processor/main.py
+++ ArchV2/Content-Processor/main.py
@@ -33,7 +33,7 @@ def create_unique_id(unique_ids:set) -> int:
     return id
 
 def worker(feed_url):
-    logging.info(f'Processing feed {feed_url}')
+    logging.debug(f'Processing feed {feed_url}')
     return process_feed(feed_url)
 
 def insert_database(article_dict:dict, table_name:str):
@@ -57,6 +57,8 @@ os.system(f"aws s3 cp s3://{bucket}/{rss_file} /home/ec2-user/{rss_file}")
 with open(f'/home/ec2-user/{rss_file}', 'r') as file:
     rss_feeds = json.load(file)
 
+rss_feeds = list(set(rss_feeds)) # Removing Duplicates if they exist.
+logging.info(f"Amount of rss_feeds before processing: {len(rss_feeds)}")
 ##### Load in article IDS
 article_id_file = 'unique_article_ids.csv'
 os.system(f"aws s3 cp s3://{bucket}/{article_id_file} /home/ec2-user/{article_id_file}")
@@ -75,7 +77,7 @@ for feed in rss_feeds:
         FEEDS.append(feed)
 
 if testing:
-    FEEDS = FEEDS[:90]    
+    FEEDS = FEEDS[-90:]    
 
 with multiprocessing.Pool(processes=multiprocessing.cpu_count()) as pool:
     content_archive = pool.map(worker, FEEDS)
@@ -110,6 +112,7 @@ for output in tqdm(content_archive, total=len(content_archive)):
         insert_database(article, 'articleContent')
         logging.info(f"Finished Processing Article {article['articleID']}")
 
+logging.info(f"Amount of rss_feeds after processing: {len(rss_feeds)}")
 logging.info(f"Amount of Articles Processed: {len(content_lake)}")
 ############## Save Data ##############
 
@@ -149,6 +152,7 @@ if not new_df.empty: # Check if any new articles even exist.
     os.system(f"aws s3 cp /home/ec2-user/content-lake.json s3://toast-daily-content/content-lake.json")
 
 # #### Save RSS Feed back to S3
+rss_feeds = list(set(rss_feeds)) # Removing Duplicates if they exist.
 with open(f'/home/ec2-user/{rss_file}', 'w') as file:
     json.dump(rss_feeds, file, indent=4)
 os.system(f"aws s3 cp /home/ec2-user/{rss_file} s3://{bucket}/{rss_file}")
