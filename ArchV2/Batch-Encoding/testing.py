d0b7c4e45528a0147223fc745e0b494026906586
Charles E. Gormley <76138796+Charles-Gormley@users.noreply.github.com>
2023-12-06 01:38:09 -0500

    little change guy

--- ArchV2/Batch-Encoding/testing.py
+++ ArchV2/Batch-Encoding/testing.py
@@ -38,6 +38,7 @@ encoded_df = encode_dataframe_column(process_df, "content") # This needs (articl
 os.system(f"aws s3 cp s3://{bucket}/{embeddings_file} /home/ec2-user/{embeddings_file}")
 old_embeddings = torch.load(f"/home/ec2-user/{embeddings_file}")
 logging.info(f"Old Torch Tensor Size : {old_embeddings['tensor'].size}")
+old_tensor = torch.tensor(old_embeddings["tensor"])
 
 # Filter out data older than 7 days
 seven_days_ago = datetime.now() - timedelta(days=7)
@@ -49,7 +50,7 @@ new_encoded_tensor = encoded_df['tensor'].apply(lambda tensor: tensor.squeeze(0)
 
 concatenated_embeddings = {
     'articleID': torch.cat([old_data_filtered['articleID'], new_article_id_tensor]),
-    'tensor': torch.cat([old_data_filtered['tensor'], new_encoded_tensor]),
+    'tensor': torch.cat([old_tensor, new_encoded_tensor]),
     'unixTime': torch.cat([old_data_filtered['unixTime'], new_article_id_time])
 }
 logging.info("Existing embeddings loaded and concatenated with new embeddings.")
