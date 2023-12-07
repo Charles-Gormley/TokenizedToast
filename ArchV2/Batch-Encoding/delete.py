61fd37bf76c7abc346385b394239ec8adb2f2ab0
Charlie <ceg98@drexel.edu>
2023-12-06 23:37:30 -0500

    testing

--- ArchV2/Batch-Encoding/delete.py
+++ ArchV2/Batch-Encoding/delete.py
@@ -4,6 +4,7 @@ import logging
 import pandas as pd
 import numpy as np
 import torch
+from encoder import encode_dataframe_column
 
 from datetime import datetime, timedelta
 bucket = "toast-encodings"
@@ -14,9 +15,11 @@ os.system(f"aws s3 cp s3://toast-daily-content/content-lake.json /home/ec2-user/
 with open(f'/home/ec2-user/content-lake.json', 'r') as file:
     old_content_lake = json.load(file)
 
-encoded_df = pd.DataFrame(old_content_lake)
-encoded_df['to_encode'] = True
+content_df = pd.DataFrame(old_content_lake)
+content_df['to_encode'] = True
 
+
+encoded_df = encode_dataframe_column(content_df, "content")
 
 new_encoded_tensor = np.array(encoded_df['tensor'].apply(lambda tensor: tensor.squeeze(0)))
 new_article_id_time = torch.tensor(encoded_df['unixTime'].values)
@@ -31,8 +34,8 @@ concatenated_embeddings = {
 torch.save(concatenated_embeddings, f"/home/ec2-user/{embeddings_file}")
 os.system(f"aws s3 cp /home/ec2-user/{embeddings_file} s3://{bucket}/{embeddings_file}")
 
-encoded_df["to_encode"] = False
-content_lake_dict = encoded_df.to_dict(orient='records')
+content_df["to_encode"] = False
+content_lake_dict = content_df.to_dict(orient='records')
 logging.debug(f"Length of Concatenated Dictionary: {len(content_lake_dict)}")
 with open(f'/home/ec2-user/content-lake.json', 'w') as file:
     json.dump(content_lake_dict, file, indent=4)
