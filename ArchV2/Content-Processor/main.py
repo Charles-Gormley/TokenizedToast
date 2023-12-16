2639dc7bda81001635af042613cc12d7020e0d7f
Charles E. Gormley <76138796+Charles-Gormley@users.noreply.github.com>
2023-12-16 03:52:56 -0500

    seeing if this is the issue

--- ArchV2/Content-Processor/main.py
+++ ArchV2/Content-Processor/main.py
@@ -75,8 +75,8 @@ with open(f'/home/ec2-user/{article_id_file}', 'r') as file:
 content_lake = []
 FEEDS = []
 for feed in rss_feeds:
-    if feed['update']:
-        FEEDS.append(feed)
+    # if feed['update']:
+    FEEDS.append(feed)
 
 if testing:
     FEEDS = FEEDS[-90:]    
